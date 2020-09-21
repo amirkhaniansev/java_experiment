8ffe056e0f6d32a677ea401cd5a84b6476a61ba9
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2020-09-21 15:13:51 +0400

    added user table script

---
+++ db/tables/user.sql
@@ -0,0 +1,8 @@
+ 
+CREATE TABLE userDB.user (
+    id              INT             NOT NULL AUTO_INCREMENT,
+    age             INT             NOT NULL,
+    name            VARCHAR(2000)   NOT NULL CHARSET utf8
+
+    CONSTRAINT PK_USER_ID      PRIMARY KEY (ID)
+);
