bb22aadea32d86cd9a33e681ee202bc7eeb1244e
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2020-09-23 23:19:58 +0400

    added non-unique index for name

--- db/tables/user.sql
+++ db/tables/user.sql
@@ -1,8 +1,9 @@
- 
 CREATE TABLE userDB.user (
     id              INT             NOT NULL AUTO_INCREMENT,
     age             INT             NOT NULL,
-    name            VARCHAR(2000)   NOT NULL CHARSET utf8
+    name            VARCHAR(255)   CHARSET utf8 NOT NULL,
 
-    CONSTRAINT PK_USER_ID      PRIMARY KEY (ID)
+    CONSTRAINT PK_USER_ID      PRIMARY KEY (id)
 );
+
+CREATE INDEX IX_NAME ON userDB.user (name);
