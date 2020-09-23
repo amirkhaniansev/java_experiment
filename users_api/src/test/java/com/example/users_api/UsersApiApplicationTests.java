a0e14a129729dc0f7f684f417f32cf1335834633
Sevak Amirkhanian <amirkhanyan.sevak@gmail.com>
2020-09-23 23:03:22 +0400

    merged PR : Test (#15)
    * added tests for GET api/v1/users
    
    * changed the URLs
    
    * added test for POST users

--- users_api/src/test/java/com/example/users_api/UsersApiApplicationTests.java
+++ users_api/src/test/java/com/example/users_api/UsersApiApplicationTests.java
@@ -1,13 +1,56 @@
 package com.example.users_api;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class UsersApiApplicationTests {
 
+	@Autowired
+	private UserController controller;
+	
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
+	@Test
+	void testGetUsers() throws Exception {
+		this.mockMvc
+			.perform(get("/api/v1/user/?name=Sevak"))
+			.andDo(print())
+			.andExpect(status().isOk());
+
+		this.mockMvc
+			.perform(get("/api/v1/user/?name=Sevak&page=0"))
+			.andDo(print())
+			.andExpect(status().isOk());
+		
+		this.mockMvc
+			.perform(get("/api/v1/user/?name=Sevak&page=0&count=2"))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
+
+	@Test
+	void testPostUsers() throws Exception {
+		this.mockMvc
+			.perform(post("{\"name\":\"Alice\", \"age\" :14 }"))
+			.andDo(print())
+			.andExpect(status().isOk())
+			.andExpect(content().string(containsString("{\"name\":\"Alice\", \"age\" :14 }")));
+	}
 }
---
+++ users_api/src/test/java/com/example/users_api/UsersApiTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.users_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApiTestApplication.class, args);
+	}
+}
