f5419d218a91ed2f895812fc5ca6178d7dbcf82a
amirkhaniansev <amirkhanyan.sevak@gmail.com>
2020-09-23 22:54:23 +0400

    added tests for GET api/v1/users

--- users_api/src/test/java/com/example/users_api/UsersApiApplicationTests.java
+++ users_api/src/test/java/com/example/users_api/UsersApiApplicationTests.java
@@ -1,13 +1,44 @@
 package com.example.users_api;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 class UsersApiApplicationTests {
 
+	@Autowired
+	private UserController controller;
+	
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
+	@Test
+	void getUsers() throws Exception {
+		this.mockMvc
+			.perform(get("/?name=Sevak"))
+			.andDo(print())
+			.andExpect(status().isOk());
+
+		this.mockMvc
+			.perform(get("/?name=Sevak&page=0"))
+			.andDo(print())
+			.andExpect(status().isOk());
+		
+		this.mockMvc
+			.perform(get("/?name=Sevak&page=0&count=2"))
+			.andDo(print())
+			.andExpect(status().isOk());
+	}
 }
